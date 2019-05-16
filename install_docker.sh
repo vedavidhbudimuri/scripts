0d9f143c3e1cb3249bb67cfcedf252faf1644b60
vedavidhbudimuri <vedavidhbudimuri@gmail.com>
2019-05-16 14:35:49 +0530

    install_docker.sh

---
+++ install_docker.sh
@@ -0,0 +1,10 @@
+ sudo apt update
+ apt list --upgradable
+ sudo apt update
+ sudo apt -y upgrade
+ sudo apt install -y docker.io
+ sudo groupadd docker
+ sudo usermod -aG docker $USER
+ sudo apt install -y python-pip
+ sudo pip install docker-compose
+ 
